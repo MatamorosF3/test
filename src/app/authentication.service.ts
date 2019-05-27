2fe60739ea2964880cf87856e51c2a93f0d3fa63
Francisco Matamoros <fmatamoros@HN-LNOHN0209m.local>
2019-05-27 02:36:39 -0600

    MOD

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -25,7 +25,7 @@ export class AuthenticationService {
 
     login(username: string, password: string) {
         console.log("Api URL");
-        console.log(apiUrl);
+        console.log(this.apiUrl);
 
         return this.http.post<any>(`${this.apiUrl}/login`, { username, password })
             .pipe(map(user => {
